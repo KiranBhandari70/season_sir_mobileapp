d45cf28f67ce5ab5c3fc81f785342bcfac49616f
KiranBhandari70 <kiran.bhandariofficial70@gmail.com>
2025-09-15 19:32:47 +0545

    First commit for login and signup page

---
+++ android/app/src/main/kotlin/com/pcpstudent/season_sir_mobileapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.pcpstudent.season_sir_mobileapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import "package:season_sir_mobileapp/loginpage.dart";
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PCPS DEMO',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:  loginpage(),
+    );
+  }
+}
