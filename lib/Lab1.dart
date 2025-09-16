84ea8d7c47e18b3a447e69b91e605ee7af20a330
KiranBhandari70 <kiran.bhandariofficial70@gmail.com>
2025-09-16 19:17:09 +0545

    "Scaffold- the main backpage of the app where other components are added"

---
+++ lib/Lab1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Lab1 extends StatefulWidget {
+  const Lab1({super.key});
+
+  @override
+  State<Lab1> createState() => _Lab1State();
+}
+
+class _Lab1State extends State<Lab1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
